962ac6d6c7eccc7f63111b6db631f61fa0fe6562
AlexKing <pikashev171@mail.ru>
2022-08-07 01:00:56 +0500

    My commit

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -3,6 +3,7 @@ package jm.task.core.jdbc;
 import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
 import jm.task.core.jdbc.util.Util;
 
@@ -11,9 +12,7 @@ import static jm.task.core.jdbc.util.Util.getSessionFactor;
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгорит
-        UserServiceImpl userService = new UserServiceImpl();
-
-
+        UserService userService = new UserServiceImpl();
 
         userService.createUsersTable();
 
