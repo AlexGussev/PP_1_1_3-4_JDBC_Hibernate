994fa1dcca45affc3e5a6d0416de17a10295eab3
AlexKing <pikashev171@mail.ru>
2022-08-07 01:17:36 +0500

    my commit

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,14 +1,13 @@
 package jm.task.core.jdbc;
 
-import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
-import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
 import jm.task.core.jdbc.util.Util;
 
 public class Main extends Util {
     public static void main(String[] args) {
         // реализуйте алгорит
-        UserServiceImpl userService = new UserServiceImpl();
+        UserService userService = new UserServiceImpl();
         userService.createUsersTable();
 
         userService.saveUser("Name1", "LastName1", (byte) 20);
